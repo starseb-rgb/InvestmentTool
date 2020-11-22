58a5b7233148b66435e9c6bd8a11b88c5b6078ce
starseb-rgb <seb.basti3@web.de>
2020-11-22 20:27:58 +0100

    deleted folder 'mainApproach' because of issues with importing function of another class created main function and created first test object added mock data for timestamps

---
+++ main.py
@@ -0,0 +1,10 @@
+from createChart import Chart
+
+if __name__=="__main__":
+
+    btc = Chart('bitcoin', 'eur', '1448150400', '1606003200')
+    btc.createChart()
+
+    # sample data: 01/01/2020 --> 1577836800    01/02/2020 --> 1580515200
+    # sample data: 01/11/2020 --> 1604188800    22/11/2020 --> 1606003200
+    # sample data: 22/11/2015 --> 1448150400
