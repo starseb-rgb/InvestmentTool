efdcd3c857601d0384e8d980c52c017455391e12
sebastianstarzetz <sebastian.starzetz@web.de>
2020-12-24 00:20:27 +0100

    with this commit we have our first prototype finished. - connected previous code with our GUI - display data as an interactive chart instead of 'fixed' matplotlib chart

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ from createChart import Chart
 if __name__=="__main__":
 
     btc = Chart('bitcoin', 'eur', '1448150400', '1606003200', 15, 200)
-    btc.createChart()
+
+
+    arr = btc.createChart()
+    print(arr[0])
+
 
     # sample data: 01/01/2020 --> 1577836800    01/02/2020 --> 1580515200
     # sample data: 01/11/2020 --> 1604188800    22/11/2020 --> 1606003200
