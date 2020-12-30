d9b03ffe887f9f26f636435520b9c8863f1bde50
sebastianstarzetz <sebastian.starzetz@web.de>
2020-12-30 14:25:26 +0100

    added function to button on loading screen added some comments removed unnecessary parts

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import menu_choose
 from loading_screen import LoadingScreen
-from PyQt5 import QtCore, QtGui, QtWidgets
+# from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QDialog, QApplication, QPushButton, QVBoxLayout
 import sys
 
@@ -11,21 +11,23 @@ if __name__ == "__main__":
     # start application
     app = QApplication(sys.argv)
 
+    # if the next windows are not appearing if you press any buttons than remove all of the # below
+    # this will start all the files manually from this main file
+
     # create main window and show it
     # main = menu_choose.Chart_GUI()
     # main.show()
 
     # create chart window and show it
-    MainWindow = QtWidgets.QMainWindow()
-    chart = menu_choose.Ui_Menu_Choose()
-    chart.setupUi(MainWindow)
-    MainWindow.show()
+    # MainWindow = QtWidgets.QMainWindow()
+    # chart = menu_choose.Ui_Menu_Choose()
+    # chart.setupUi(MainWindow)
+    # MainWindow.show()
 
     # create loading screen and show it
-    MainWindow2 = QtWidgets.QMainWindow()
     load = LoadingScreen()
-    load.setup(MainWindow2)
-    MainWindow2.show()
+    load.startLoadingScreen()
+
 
     sys.exit(app.exec_())
 
